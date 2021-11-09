2bacccb5ff4710cde3f9627716bedb1a2966a3de
quynhtrang1999 <63849775+quynhtrang1999@users.noreply.github.com>
2021-11-09 11:56:37 +0700

    edit home

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,23 +5,21 @@ import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-    {
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
         path: '',
-        component: HomeComponent,
-        children: [
-            {
-                path: '',
-                component: MainComponent
-            }
-        ]
-    }
-]
+        component: MainComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-    declarations: [],
-    imports: [
-        RouterModule.forChild(routes)
-    ],
-    exports: [RouterModule]
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class HomeRoutingModule {}
