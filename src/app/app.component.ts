3a2c4ed5b7dc1672d4cc285ee31673f925fd0e68
Jay Ngo <thanhdat8799@gmail.com>
2021-11-13 18:21:34 +0700

    implement login persistent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './core/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mockup-project';
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    if (localStorage.getItem('token')) {
+      this.userService.getUpdate(true);
+      console.log('im run')
+    }
+  }
 }
