376d0d67f68887a9830fe5ff833433eec285db87
quynhtrang1999 <63849775+quynhtrang1999@users.noreply.github.com>
2021-11-16 00:15:57 +0700

    fix like profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,8 +12,13 @@ export class ProfileComponent implements OnInit {
   username!: string;
   bio!: string;
   image!: string;
-  isMe: boolean;
-  profile;
+  isMe!: boolean;
+  profile: any;
+  myPosts: any;
+  favoPosts: any;
+  lengthMyPosts!: number;
+  lengthFavoPosts!: number;
+  isFavorited: any;
   defaultImg= "https://cdn.iconscout.com/icon/free/png-256/account-avatar-profile-human-man-user-30448.png";
   constructor(
     private authService: AuthService,
@@ -28,13 +33,20 @@ export class ProfileComponent implements OnInit {
       this.authService.getCurrentUser().subscribe(user=> {
         this.isMe = this.username == user.user.username;
         console.log(this.isMe ? "yes, its mah profile" : "nah, not my profile" + "its: " + this.username)
-      }) 
-      console.log(this.username)
+      })
       this.authService.getProfile(this.username).subscribe((data) => {
         this.image = data.profile['image'] || this.defaultImg;
         this.bio = data.profile['bio'];
         this.profile = data.profile;
       })
+    })
+    this.articlesService.getMyArticle(this.username).subscribe((data) => {
+      this.myPosts = data.articles;
+      this.lengthMyPosts = this.myPosts.length;
+    })
+    this.articlesService.getFavoriteArticle(this.username).subscribe((data) => {
+      this.favoPosts = data.articles;
+      this.lengthFavoPosts = this.favoPosts.length;
     })
   }
 
@@ -50,5 +62,55 @@ export class ProfileComponent implements OnInit {
         this.profile.following = !this.profile.following
       })
     }
+  }
+  handleLike(post: any) {
+    const slug = post.slug;
+    if(post.favorited == false){
+      this.articlesService.PostFavorited(slug).subscribe(data => {
+        const article = data;
+        for(let i = 0; i < this.myPosts.length; i++){
+          if(this.myPosts[i].slug == article.article.slug){
+            this.myPosts[i] = article.article;
+            break;
+          }
+        }
+        for(let i = 0; i < this.favoPosts.length; i++){
+          if(this.favoPosts[i].slug == article.article.slug){
+            this.favoPosts[i] = article.article;
+            break;
+          }
+        }
+        this.articlesService.getFavoriteArticle(this.username).subscribe((data) => {
+          this.favoPosts = data.articles;
+          this.lengthFavoPosts = this.favoPosts.length;
+        })
+      })
+    }
+    else{
+      this.articlesService.DeleteFovorited(slug).subscribe(data =>{
+        const article = data;
+        for(let i = 0; i< this.myPosts.length; i++){
+          if(this.myPosts[i].slug == article.article.slug){
+            this.myPosts[i] = article.article;
+            break;
+          }
+        }
+        for(let i = 0; i< this.favoPosts.length; i++){
+          if(this.favoPosts[i].slug == article.article.slug){
+            this.favoPosts.splice(i, 1);
+            break;
+          }
+        }
+      })
+    }
+  }
+  handleDelete(post: any){
+    this.articlesService.deleteArticle(post.slug).subscribe(data => {
+      console.log("delete success" + data)
+      this.articlesService.getMyArticle(this.username).subscribe((data) => {
+        this.myPosts = data.articles;
+        this.lengthMyPosts = this.myPosts.length;
+      })
+    })
   }
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
-import { ArticleDetailComponent } from '../home/article-detail/article-detail.component';
-import { MyArticlesComponent } from './my-articles/my-articles.component';
-import { FavoriteArticleComponent } from './favorite-article/favorite-article.component';
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    MyArticlesComponent,
-    FavoriteArticleComponent
   ],
   imports: [ProfileRoutingModule, CommonModule],
 })
